d18ca4b3b70b81582e3a9bd1cd937868227f65e7
Cai66lin <3150828211@qq.com>
2024-09-01 16:08:49 +0800

    地址簿功能代码

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -47,7 +47,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         registry.addInterceptor(jwtTokenUserInterceptor)
                 .addPathPatterns("/user/**")
                 .excludePathPatterns("/user/user/login")
-                .excludePathPatterns("/user/shop.status");
+                .excludePathPatterns("/user/shop/status");
     }
 
     /**
