8eb6beea4490875b5b2bb694a566817e53deb58e
Cai66lin <3150828211@qq.com>
2024-10-10 23:32:13 +0800

    配置更新

--- caizhidao-server/src/main/java/com/caizhidao/config/OssConfiguration.java
+++ caizhidao-server/src/main/java/com/caizhidao/config/OssConfiguration.java
@@ -17,7 +17,6 @@ public class OssConfiguration {
     @Bean
     @ConditionalOnMissingBean
     public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties){
-        log.info("开始创建阿里云文件上传工具类对象：{}", aliOssProperties);
         return new AliOssUtil(aliOssProperties.getEndpoint(),
                 aliOssProperties.getAccessKeyId(),
                 aliOssProperties.getAccessKeySecret(),
